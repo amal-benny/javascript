aab43d7c4681c870ddcf85bdc7db8b88243cad9f
amal-benny <amalbennyadgjmp@gmail.com>
2024-02-26 22:09:46 +0530

    Refreshing just the logics that  are being used in the javascript to master the fundamentals of react

---
+++ BasicsForReact/variables.js
@@ -0,0 +1,12 @@
+let stringVar = "Hello, world!";
+let numberVar = 42;
+let booleanVar = true;
+let arrayVar = [1, 2, 3, 4, 5];
+let object = { name: "John", age: 30, city: "New York" };
+
+
+console.log("String Variable:", stringVar);
+console.log("Number Variable:", numberVar);
+console.log("Boolean Variable:", booleanVar);
+console.log("Array Variable:", arrayVar);
+console.log("Object Variable:", object.name);
